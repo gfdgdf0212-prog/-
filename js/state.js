dc2e858dc5ff7bfdaee34fcfde3d86692506cb7a
gfdgdf0212-prog <gfdgdf0212@gmail.com>
2026-08-05 13:09:24 +0300

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,7 @@
+import { CONFIG, KEY, ABIL_BY_K, MUTS, TREE_SKINS } from './config.js';
+import { safeInt } from './utils.js';
+
+export function maxHpOf(o){ /* … */ }
+export function load(){ /* …возвращает объект S… */ }
+export const S = load();
+export function save(){ /* … */ }
